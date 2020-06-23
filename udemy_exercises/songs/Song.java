84d1b5b2329f1286db105ffc2f10f638f0cc5778
gabe chavez <gabe.chavez77@gmail.com>
2020-06-22 20:39:50 -1000

    completed songs exercise

--- udemy_exercises/songs/Song.java
+++ udemy_exercises/songs/Song.java
@@ -1,8 +1,24 @@
 package udemy_exercises.songs;
 
-class Song {
+public class Song {
+    private String title;
+    private double duration;
 
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return this.title + ": " + this.duration;
+    }
 }
+
 
 /*
 **Song Player w/ Java Classes**
