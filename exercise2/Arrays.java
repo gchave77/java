193ab7130c311dc91bcf84f52f97d7e07c57ad5d
gabe chavez <gabe.chavez77@gmail.com>
2020-06-23 22:17:56 -1000

    finish Arrays

--- exercise2/Arrays.java
+++ exercise2/Arrays.java
@@ -4,12 +4,28 @@ class Arrays {
     //Take the arrays.js file, and refactor the code into Java in this file.
     public static void main(String[] args) {
         int[] nums = new int[]{10, 23, 3, 4, 5, 2, 1};
-        System.out.println("The integer array is: " + java.util.Arrays.toString(nums));
         String[] greetings = new String[]{"hello", "howdy", "hallo", "hola", "bonjour", "ciao", "أهلا"};
-        System.out.println("The String array is: " + java.util.Arrays.toString(greetings));
         int[] binaries = new int[]{0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0};
-        System.out.println("The binary array is: " + java.util.Arrays.toString(binaries));
+        printNums(nums);
+        printGreetings(greetings);
+        printBinaries(binaries);
     }
+    public static void printNums(int[] nums) {
+        System.out.println("The integer array is: " + java.util.Arrays.toString(nums));
+    };
+    public static void printGreetings(String[] greetings) {
+        System.out.println("The String array is: " + java.util.Arrays.toString(greetings));
+    };
+    public static void printBinaries(int[] binaries) {
+        System.out.println("The binary array is: " + java.util.Arrays.toString(binaries));
+        for (int value : binaries) {
+            if (value == 0) {
+                System.out.println("false");
+            } else {
+                System.out.println("true");
+            }
+        }
+    };
 }
 
 //    void printNums() {};
--- exercise2/Colors.java
+++ exercise2/Colors.java
@@ -3,7 +3,9 @@ package exercise2;
 public class Colors {
 
     String[] colorful(String[] strings) {
-        // For the given strings, keep all the strings that begin with a color ("red", "yellow", "green", "blue", etc.) return an array of strings that start with a color.  discard all the other strings.  If no strings start with a color in the input array, return an empty array.
+        // For the given strings, keep all the strings that begin with a color ("red", "yellow", "green", "blue", etc.)
+        // return an array of strings that start with a color.
+        // discard all the other strings.  If no strings start with a color in the input array, return an empty array.
         return new String[]{};
     }
 
