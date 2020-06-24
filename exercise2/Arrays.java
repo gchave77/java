8d4a094a62b334b0031f3606f14879d88b3a17c8
gabe chavez <gabe.chavez77@gmail.com>
2020-06-23 19:01:33 -1000

    completed Arrays.java

--- exercise2/Arrays.java
+++ exercise2/Arrays.java
@@ -2,18 +2,16 @@ package exercise2;
 
 class Arrays {
     //Take the arrays.js file, and refactor the code into Java in this file.
-    int[] nums;
-    String[] greetings;
-    boolean[] binaries;
-
-    void printNums() {};
-    void printGreetings() {};
-    void printBinaries() {};
+    public static void main(String[] args) {
+        int[] nums = new int[]{10, 23, 3, 4, 5, 2, 1};
+        System.out.println("The integer array is: " + java.util.Arrays.toString(nums));
+        String[] greetings = new String[]{"hello", "howdy", "hallo", "hola", "bonjour", "ciao", "أهلا"};
+        System.out.println("The String array is: " + java.util.Arrays.toString(greetings));
+        int[] binaries = new int[]{0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0};
+        System.out.println("The binary array is: " + java.util.Arrays.toString(binaries));
+    }
 }
 
-
-
-
-
-
-
+//    void printNums() {};
+//    void printGreetings() {};
+//    void printBinaries() {};
